e24560138eedb16d10af267982ffa9c3ca6f54df
Anastasiya Sukhorukova <anastasiyaaa11@mail.ru>
2022-12-26 22:50:05 +0300

    исправила класс ошибки

--- scripts/index.js
+++ scripts/index.js
@@ -192,7 +192,7 @@ enableValidation({
   inputSelector: '.popup__input',
   submitButtonSelector: '.popup__save',
   inactiveButtonClass: 'popup__save_disabled',
-  inputErrorClass: 'popup__input_type_error',
+  inputErrorClass: 'popup__input-error',
   errorClass: 'popup__error_visible'
 }); 
 
